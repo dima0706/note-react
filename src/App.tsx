9066a4a85004124aef1e8d551a32c1054ae43745
dimaliang <564074944@qq.com>
2022-03-30 21:23:22 +0800

    upd router-mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import '@/App.less';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { rootRoutes } from '@/routes';
 
 export default function App() {
   return (
     <div className="App">
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           {rootRoutes.map((item) => (
             <Route key={item.key} path={item.path} element={item.component}></Route>
           ))}
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
