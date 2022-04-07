4a740cddba3d868d5bd6b28d56d14879440c215e
dimaliang <564074944@qq.com>
2022-04-07 22:22:01 +0800

    添加log及body-parser

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,19 @@
 const Koa = require('koa');
 const app = new Koa();
 const router = require('./router');
+const bodyParser = require('koa-bodyparser');
+
+app.use(bodyParser());
+
+app.use(async (ctx, next) => {
+  if (ctx.request.method === 'POST') {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url} => ${JSON.stringify(ctx.request.body)}`);
+  } else {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}`);
+  }
+  await next();
+});
 
 app.use(router.routes());
-app.use(router.allowedMethods());
 
 app.listen(3000);
